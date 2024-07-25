f4a4a44018d667bb8ea0470e2bf8e44ef6184102
inaemin <inaemin@gmail.com>
2024-07-25 21:39:54 +0900

    bj17413 commit

--- bj/src/bj17413/Main.java
+++ bj/src/bj17413/Main.java
@@ -1,38 +1,52 @@
 package bj17413;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class Main {	
 	public static void main(String[] args) {
-		// start_idx와 end_idx를 찾는다.
-		// 그 부분만 reverse해서 새로운 String을 만든다.
 		// 태그는 <> 사이의 부분 문자열. 알파벳 소문자와 공백만 있음.
 		// 단어는 알파벳 소문자와 숫자로 이루어진 부분 문자열. 연속하는 두 단어는 공백 하나로 구분.
 		// 태그와 단어 사이에는 공백이 없음.
 		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
+		String str = sc.nextLine();
+		StringBuilder answer = new StringBuilder();
+		StringBuilder word = new StringBuilder();
 		boolean isTag = false;
-		int[] idx = new int[2];
-		List<int[]> arr = new ArrayList<>();
-		String answer = "";
+		
 		for (int i=0; i<str.length(); i++) {
 			char ch = str.charAt(i);
+			
 			if (ch == '<') {
+				// 태그가 시작되면 이전에 쌓인 단어를 뒤집어서 추가
+				if (word.length() > 0) {
+					answer.append(word.reverse());
+					word.setLength(0);						
+				}
 				isTag = true;
-				answer += ch;
+				answer.append(ch);
 			} else if (ch == '>') {
+				// 태그가 끝나면 플래그를 바꿈.
 				isTag = false;
-				answer += ch;
-			} else if ()
-			
-			
+				answer.append(ch);
+			} else if (Character.isWhitespace(ch)) {
+				// isTag가 false이고, 공백을 만나면 이전에 쌓인 단어를 뒤집어서 추가
+				if (word.length() > 0 && !isTag) {
+					answer.append(word.reverse());
+					word.setLength(0);						
+				}
+				answer.append(ch);
+			} else if (!isTag && (Character.isLowerCase(ch) || Character.isDigit(ch))) {
+				word.append(ch);
+			} else {
+				answer.append(ch);
+			}
 		}
-		for (int[] ele : arr) {
-			System.out.println(Arrays.toString(ele));
+		// 마지막에 쌓인 단어를 뒤집어서 추가.
+		if (word.length() > 0 && !isTag) {
+			answer.append(word.reverse());
+			word.setLength(0);						
 		}
+		System.out.println(answer);
 		sc.close();
 	}
 }
