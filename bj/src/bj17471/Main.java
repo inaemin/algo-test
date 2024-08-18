8d7f14e28cef3b7dea6f12ef2272de83f6101835
inaemin <inaemin@gmail.com>
2024-08-18 18:03:18 +0900

    bj17471 commit

---
+++ bj/src/bj17471/Main.java
@@ -0,0 +1,22 @@
+package bj17471;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		// 구역의 개수
+		int N = sc.nextInt();
+		// 구역의 인구
+		int[] ppl = new int[N];
+		for (int i=0; i<N; i++) {
+			ppl[i] = sc.nextInt();
+		}
+		// 그 구역와 인접한 구역의 수, 인접한 구역의 번호.
+		for (int i=0; i<N; i++) {
+			
+		}
+		
+		sc.close();		
+	}
+}
