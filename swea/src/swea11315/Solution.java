b19d3614df488bc938f1cc0aa1795f47b30c0a63
inaemin <inaemin@gmail.com>
2024-08-18 10:27:51 +0900

    swea 11315 commit

--- swea/src/swea11315/Solution.java
+++ swea/src/swea11315/Solution.java
@@ -1,7 +1,66 @@
 package swea11315;
 
+import java.util.Scanner;
+
 public class Solution {
-	public static void main(String[] args) {
+	
+	private static boolean isOmokSuccess(char[][] board, int i, int j) {
+		int[] dx = {1, 1, 1, 0};
+		int[] dy = {-1, 0, 1, 1};
 		
+		for (int k=0; k<4; k++) {
+			int x = i;
+			int y = j;
+			boolean result = true;
+			for (int l=0; l<4; l++) {
+				x += dx[k];
+				y += dy[k];
+				if (x < 0 || x >= board.length) {
+					result = false;
+					break;					
+				}
+				if (y < 0 || y >= board.length) {
+					result = false;
+					break;
+				}
+				if (board[x][y] != 'o') {
+					result = false;
+					break;
+				}
+			}
+			if (result)
+				return true;
+		}
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int t=1; t<=T; t++) {
+			int N = sc.nextInt();
+			// o는 돌이 있는 칸, .은 돌이 없는 칸
+			char[][] board = new char[N][N];
+			for (int i=0; i<N; i++) {
+				String line = sc.next();
+				for (int j=0; j<N; j++) {
+					board[i][j] = line.charAt(j);
+				}
+			}
+			
+			boolean result = false;
+			out: for (int i=0; i<N; i++) {
+				for (int j=0; j<N; j++) {
+					if (board[i][j] == 'o' && isOmokSuccess(board, i, j)) {
+						result = true;
+						break out;
+					}
+				}
+			}
+			
+			// 출력
+			System.out.println("#"+t+" "+(result ? "YES" : "NO"));
+		}
+		sc.close();
 	}
 }
