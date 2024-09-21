2b62dd97bc16a0034b741c79aeb1926eb3e36600
inaemin <inaemin@gmail.com>
2024-09-21 15:57:04 +0900

    bj1676

--- bj/src/bj1676/Main.java
+++ bj/src/bj1676/Main.java
@@ -1,8 +1,22 @@
 package bj1676;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		// 약수로 10을 만들 수 있는 개수를 찾으면 됨
-		// 소인수분해
+		// 2보다 5가 더 적기 때문에 약수 중 5의 개수만 세면 됨.
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int count = 0;
+		int k=5;
+		while (k <= N) {
+			count += N/k;
+			k *= 5;
+		}
+		
+		System.out.println(count);
+		
+		sc.close();
 	}
 }
