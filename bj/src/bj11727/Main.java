2762d13f992d0bb52f76b6529745d807135d9ebe
inaemin <inaemin@gmail.com>
2024-09-22 21:18:10 +0900

    bj11727

---
+++ bj/src/bj11727/Main.java
@@ -0,0 +1,22 @@
+package bj11727;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] dp = new int[N+1];
+		dp[1] = 1;
+		if (N >= 2) {
+			dp[2] = 3;
+		}
+		for (int i=3; i<=N; i++) {
+			dp[i] = (2*dp[i-2] + dp[i-1]) % 10007;
+		}
+		
+		System.out.println(dp[N]);
+		
+		sc.close();
+	}
+}
